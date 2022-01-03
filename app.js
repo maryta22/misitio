162af857a6d0fcf157efc873a64ab209989112af
maryta22 <maria.mariaog.rivera@gmail.com>
2022-01-02 23:42:15 -0500

    cambios

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var servicesRouter = require('./routes/services');
 var contacusRouter = require('./routes/contacus');
-
+var loginRouter = require('/routes/login')
 
 var app = express();
 
@@ -26,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/services', servicesRouter);
 app.use('/contacus', contacusRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
