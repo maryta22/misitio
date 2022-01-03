9f55ac44063dabd91f52bfab1c753765af87ec85
maryta22 <maria.mariaog.rivera@gmail.com>
2022-01-03 00:03:14 -0500

    cambios

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+//Para el método POST
+let bd = {
+  'usuario': 'abc',
+  'contrasenia': '123'
+}
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
+
+//Para el método POST
+router.post('/validate', function (req, res, next) {
+  let usuario = req.body.user;
+  let contrasenia = req.body.password;
+
+  console.log("usuario: ", usuario)
+  console.log("contraseña: ", contrasenia)
+
+  //Validación
+  if (usuario == bd['usuario'] && contrasenia == bd['contrasenia']) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login')
+  }
+
+});
