a6863dfb6a28de5490108206c347856b3d03de4c
maryta22 <maria.mariaog.rivera@gmail.com>
2022-01-03 23:21:22 -0500

    cambios

--- app.js
+++ app.js
@@ -44,4 +44,27 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Function to handle the root path
+app.get('/', async function(req, res) {
+
+    // Access the provided 'page' and 'limt' query parameters
+    let page = req.query.page;
+    let limit = req.query.limit;
+
+    let articles = await Article.findAll().paginate({page: page, limit: limit}).exec();
+
+    // Return the articles to the rendering engine
+    res.render('index', {
+        articles: articles
+    });
+});
+
+let server = app.listen(8080, function() {
+    console.log('Server is listening on port 8080')
+});
+
 module.exports = app;
