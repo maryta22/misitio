f1f7bc3988955d0eae435dec8972145a505420dc
maryta22 <maria.mariaog.rivera@gmail.com>
2022-01-03 23:21:59 -0500

    cambios

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const bodyParser = require('body-parser');
+const url = require('url');
+const querystring = require('querystring');
+const Article = require('./models').Article;
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var servicesRouter = require('./routes/services');
